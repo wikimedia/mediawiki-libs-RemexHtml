49d5a838fb43f006058f7b98865d9907ad947502
James D. Forrester <jforrester@wikimedia.org>
2023-04-17 16:32:56 -0400

    build: Switch phan to special library mode
    Bug: T334492
    Change-Id: I3d39eb5fed268497f81ce80560e71f78205e5fae

--- .phan/config.php
+++ .phan/config.php
@@ -1,15 +1,8 @@
 <?php
 
-$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config-library.php';
 
-$cfg['directory_list'] = [
-	'src',
-	'vendor',
-	'tests'
-];
-
-$cfg['exclude_analysis_directory_list'][] = 'vendor';
-
+$cfg['directory_list'][] = 'tests';
 $cfg['exclude_file_list'][] = 'tests/phplint/autoload.php';
 
 return $cfg;
